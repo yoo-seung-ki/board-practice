d5c8753d08ded93f3d78fde8ca98b523a626f184
DESKTOP-BND0HI7\sgsg9 <ysgsl@naver.com>
2024-03-20 17:00:59 +0900

    2024 03 20 필요없어진 import 문 삭제

--- src/main/java/org/commonweb/security/SecurityConfig.java
+++ src/main/java/org/commonweb/security/SecurityConfig.java
@@ -7,9 +7,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
